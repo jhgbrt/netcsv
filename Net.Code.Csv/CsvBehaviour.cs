6c41cbd4a46c33a36c9bc9cf2571a39b90aca37b
Jeroen Haegebaert <jeroen@haegebaert.com>
2013-02-03 12:24:22 +0100

    moved CsvBehaviour to separate file

---
+++ Net.Code.Csv/CsvBehaviour.cs
@@ -0,0 +1,37 @@
+﻿namespace Net.Code.Csv
+{
+    public class CsvBehaviour
+    {
+        public static CsvBehaviour Default { get { return new CsvBehaviour(); } }
+        private readonly ValueTrimmingOptions _trimmingOptions;
+        private readonly MissingFieldAction _missingFieldAction;
+        private readonly bool _skipEmptyLines;
+
+        public CsvBehaviour(
+            ValueTrimmingOptions trimmingOptions = ValueTrimmingOptions.UnquotedOnly,
+            MissingFieldAction missingFieldAction = MissingFieldAction.ParseError,
+            bool skipEmptyLines = true)
+        {
+            _trimmingOptions = trimmingOptions;
+            _missingFieldAction = missingFieldAction;
+            _skipEmptyLines = skipEmptyLines;
+        }
+
+        public ValueTrimmingOptions TrimmingOptions
+        {
+            get { return _trimmingOptions; }
+        }
+
+        public MissingFieldAction MissingFieldAction
+        {
+            get { return _missingFieldAction; }
+        }
+
+        public bool SkipEmptyLines
+        {
+            get { return _skipEmptyLines; }
+        }
+
+        public QuotesInsideQuotedFieldAction QuotesInsideQuotedFieldAction { get; set; }
+    }
+}
--- Net.Code.Csv/CsvLayout.cs
+++ Net.Code.Csv/CsvLayout.cs
@@ -1,41 +1,5 @@
 namespace Net.Code.Csv
 {
-
-    public class CsvBehaviour
-    {
-        public static CsvBehaviour Default { get { return new CsvBehaviour(); } }
-        private readonly ValueTrimmingOptions _trimmingOptions;
-        private readonly MissingFieldAction _missingFieldAction;
-        private readonly bool _skipEmptyLines;
-
-        public CsvBehaviour(
-            ValueTrimmingOptions trimmingOptions = ValueTrimmingOptions.UnquotedOnly,
-            MissingFieldAction missingFieldAction = MissingFieldAction.ParseError, 
-            bool skipEmptyLines = true)
-        {
-            _trimmingOptions = trimmingOptions;
-            _missingFieldAction = missingFieldAction;
-            _skipEmptyLines = skipEmptyLines;
-        }
-
-        public ValueTrimmingOptions TrimmingOptions
-        {
-            get { return _trimmingOptions; }
-        }
-
-        public MissingFieldAction MissingFieldAction
-        {
-            get { return _missingFieldAction; }
-        }
-
-        public bool SkipEmptyLines
-        {
-            get { return _skipEmptyLines; }
-        }
-
-        public QuotesInsideQuotedFieldAction QuotesInsideQuotedFieldAction { get; set; }
-    }
-
     public class CsvLayout
     {
         public static CsvLayout Default { get { return new CsvLayout(); } }
