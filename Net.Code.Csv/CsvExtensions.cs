4b7fa32a106a47f1c781b1ae704ca874525553f9
Jeroen Haegebaert <jeroen@haegebaert.com>
2013-02-05 08:50:22 +0100

    nuget

--- Net.Code.Csv/CsvExtensions.cs
+++ Net.Code.Csv/CsvExtensions.cs
@@ -6,18 +6,16 @@ namespace Net.Code.Csv
 {
     public static class CsvExtensions
     {
-
         public static IDataReader ReadFileAsCsv(this string path, Encoding encoding)
         {
             var reader = new StreamReader(File.OpenRead(path), encoding);
-            var parser = new CsvParser(reader, 4096, CsvLayout.Default, CsvBehaviour.Default);
-            return new CsvDataReader(parser, Converter.Default);
+            return ReadStreamAsCsv(reader, CsvLayout.Default, CsvBehaviour.Default, Converter.Default);
         }
+
         public static IDataReader ReadFileAsCsv(this string path, Encoding encoding, CsvLayout csvLayout, CsvBehaviour csvBehaviour, Converter converter, int bufferSize = 4096)
         {
             var reader = new StreamReader(File.OpenRead(path), encoding);
-            var parser = new CsvParser(reader, bufferSize, csvLayout, csvBehaviour);
-            return new CsvDataReader(parser, converter);
+            return ReadStreamAsCsv(reader, csvLayout, csvBehaviour, converter, bufferSize);
         }
 
         public static IDataReader ReadStringAsCsv(this string input)
@@ -28,6 +26,16 @@ namespace Net.Code.Csv
         public static IDataReader ReadStringAsCsv(this string input, CsvLayout csvLayout, CsvBehaviour csvBehaviour, Converter converter, int bufferSize = 4096)
         {
             var reader = new StringReader(input);
+            return ReadStreamAsCsv(reader, csvLayout, csvBehaviour, converter, bufferSize);
+        }
+
+        public static IDataReader ReadStreamAsCsv(this TextReader reader)
+        {
+            return ReadStreamAsCsv(reader, CsvLayout.Default, CsvBehaviour.Default, Converter.Default);
+        }
+        
+        public static IDataReader ReadStreamAsCsv(this TextReader reader, CsvLayout csvLayout, CsvBehaviour csvBehaviour, Converter converter, int bufferSize = 4096)
+        {
             var parser = new CsvParser(reader, bufferSize, csvLayout, csvBehaviour);
             return new CsvDataReader(parser, converter);
         }
